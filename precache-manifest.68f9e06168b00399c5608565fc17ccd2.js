398d734ac43dc755799d1c63e51f32853356216a
Jeremy Kepf <jeremy1975@gmail.com>
2020-04-18 21:56:43 -0400

    Updates

---
+++ precache-manifest.68f9e06168b00399c5608565fc17ccd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b7f8970e2fa989f9db4754d722ff89",
+    "url": "/draftmaster/index.html"
+  },
+  {
+    "revision": "23a9fbbbcfe89d333c41",
+    "url": "/draftmaster/static/js/2.3529a6ce.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/draftmaster/static/js/2.3529a6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ab7ba0b4ffe623bdfb",
+    "url": "/draftmaster/static/js/main.b9b0b569.chunk.js"
+  },
+  {
+    "revision": "c5f2fc795541f253986e",
+    "url": "/draftmaster/static/js/runtime-main.a5c83816.js"
+  }
+]);
